e8f377652f9518a1d85a64ef05ef71f64851cb0d
薛棒棒 <xuebb3@asiainfo.com>
2025-02-18 18:09:05 +0800

    提交透视投影相机

--- 01环境搭建与three.js开发知识/视频教程源码/05第一个3d案例-透视投影相机/index.js
+++ 01环境搭建与three.js开发知识/视频教程源码/05第一个3d案例-透视投影相机/index.js
@@ -17,3 +17,14 @@ console.log("mesh", mesh);
 mesh.position.set(0, 10, 0);
 // 将网格添加到三维场景中
 scene.add(mesh);
+
+// 创建一个透视投影相机
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+console.log("camera", camera);
+// 设置相机位置
+camera.position.set(0, 0, 10);
+// camera.lookAt(0, 0, 0); // 相机视线 相机看向坐标原点
+// camera.lookAt(0, 10, 0); // 相机视线 相机看向y轴一点
+camera.lookAt(mesh.position); // 相机视线 相机看向网格模型
+// 将相机添加到三维场景中
+scene.add(camera);
